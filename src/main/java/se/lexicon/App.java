bbeb58f15474109c02c7f78c7e54a248059cdd3e
Your Name <youremail@yourdomain.com>
2022-10-11 11:31:33 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,20 @@
+package se.lexicon;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+
+        int age = 32;
+        System.out.println("Age: " + age);
+
+        double salary= 30000.00;
+        System.out.println("Salary:" + salary);
+
+        int number1= 60;
+        int number2= 20;
+        int result= number1 + number2;
+            System.out.println("Rseult:" + result);
+
+    }
+}
